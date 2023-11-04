cdcb5fbe14411b738d6f128706fc5cf1366f8aca
Terrad77 <140712108+Terrad77@users.noreply.github.com>
2023-11-04 18:56:15 +0200

    Initial commit

---
+++ modul-03/task-3.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function filterArray(numbers, value) {
+  let moreThenValue = [];
+  for (let item of numbers) {
+    if (item > value) {
+      moreThenValue.push(item);
+    }
+  }
+  return moreThenValue;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
